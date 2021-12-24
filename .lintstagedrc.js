a8ce4fbcdd9edab24ca688ec7332ae08914d8f9f
Petr Glaser <syreanis+1@gmail.com>
2021-12-24 12:17:57 +0100

    feat(lint): Update eslint ignore

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,12 @@
+const lintFiles = (filenames) => {
+    filenames.map((file) => console.log("File: ", process.cwd(), file, file.split(process.cwd().replace(/\\/g, "/"))))
+    return `yarn run lint --fix --file ${filenames
+        .map((file) => file.split(process.cwd().replace(/\\/g, "/"))[1])
+        .join(" --file ")}`
+}
+
 module.exports = {
-    "**/*.js?(x)": (filenames) =>
-        `yarn run lint --fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(" --file ")}`,
-    "**/*.ts?(x)": (filenames) =>
-        `yarn run lint --fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(" --file ")}`,
+    "**/*.js?(x)": lintFiles,
+    "**/*.ts?(x)": lintFiles,
     "**/*": "yarn prettier --write --ignore-unknown",
 }
