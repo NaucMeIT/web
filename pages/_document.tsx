d9f4139e4880f0b7fefad989a5744fa493681595
Petr Glaser <syreanis+1@gmail.com>
2022-02-14 00:03:02 +0100

    fix(fonts): Fix loading from Google Fonts.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,31 @@ export default function Document() {
     return (
         <Html lang='cs'>
             <Head>
-                <link href='https://fonts.googleapis.com/css2?family=Roboto&display=swap' rel='stylesheet' />
+                <link rel='preconnect' href='https://fonts.gstatic.com/' crossOrigin='true'></link>
+                <style
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        /* latin-ext */
+                        @font-face {
+                        font-family: 'Roboto';
+                        font-style: normal;
+                        font-weight: 400;
+                        font-display: swap;
+                        src: url(https://fonts.gstatic.com/s/roboto/v29/KFOmCnqEu92Fr1Mu7GxKOzY.woff2) format('woff2');
+                        unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+                        }
+                        /* latin */
+                        @font-face {
+                        font-family: 'Roboto';
+                        font-style: normal;
+                        font-weight: 400;
+                        font-display: swap;
+                        src: url(https://fonts.gstatic.com/s/roboto/v29/KFOmCnqEu92Fr1Mu4mxK.woff2) format('woff2');
+                        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+                        }
+                     `,
+                    }}
+                ></style>
                 <meta
                     name='Keywords'
                     content='IT, učení, školení, nauč mě, jak začít s IT, jak začít programovat, jak dělat web, jak psát web'
