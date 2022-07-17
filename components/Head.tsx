86c64c52401210d5273c258733590054287ac84c
Petr Glaser <12586960+pegak@users.noreply.github.com>
2022-07-17 11:26:06 +0200

    Update deps (#7)
    * chore(deps): update dependencies to mitigate vulnerabilities
    
    * chore(yarn): update yarn manager
    
    * chore(deps): update to next 12.2 and clerk 3
    
    * chore(deps): update eslint

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,13 +1,15 @@
 import NextHead from "next/head"
+import React from "react"
 
 type HeadProps = {
     readonly desc: string
     readonly twImg: string
     readonly fbImg: string
     readonly url: string
+    readonly children: React.ReactNode
 }
 
-const Head: React.FC<HeadProps> = (props) => (
+const Head = (props: HeadProps) => (
     <NextHead>
         <meta name='Description' key='desc' content={props.desc} />
         <meta name='twitter:description' key='tw:desc' content={props.desc} />
