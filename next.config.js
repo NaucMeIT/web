9c5c1b2969cb9d3cfd3c2483e0b264eee8fadc1c
Petr Glaser <syreanis+1@gmail.com>
2022-07-17 13:43:11 +0200

    feat(mdx): add mdx render example

--- next.config.js
+++ next.config.js
@@ -1,22 +1,34 @@
 const withPWA = require("next-pwa")
 
-/** @type {import('next').NextConfig} */
-module.exports = withPWA({
-    reactStrictMode: true,
-    eslint: {
-        dirs: ["pages", "components", "hooks", "utils"],
-    },
-    pwa: {
-        dest: "public",
-        disable: process.env.NODE_ENV === "development",
-    },
-    experimental: {
-        runtime: "experimental-edge",
-        images: {
-            allowFutureImage: true,
-        },
-    },
-    images: {
-        formats: ["image/avif", "image/webp"],
+const withMDX = require("@next/mdx")({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+        // providerImportSource: "@mdx-js/react",
     },
 })
+
+/** @type {import('next').NextConfig} */
+module.exports = withMDX(
+    withPWA({
+        reactStrictMode: true,
+        eslint: {
+            dirs: ["pages", "components", "hooks", "utils"],
+        },
+        pwa: {
+            dest: "public",
+            disable: process.env.NODE_ENV === "development",
+        },
+        experimental: {
+            runtime: "experimental-edge",
+            images: {
+                allowFutureImage: true,
+            },
+        },
+        images: {
+            formats: ["image/avif", "image/webp"],
+        },
+        pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+    }),
+)
