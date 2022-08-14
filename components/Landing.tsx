08d6c72fa164df304c9e7ca7015f555d2c2510ae
Petr Glaser <syreanis+1@gmail.com>
2022-08-14 15:31:36 +0200

    feat(company): wip page for companies

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,35 +1,37 @@
-import { LearnEarn, PayConsultancy, Time, Worldwide } from "./icons"
-import { Button } from "./Button"
-import { CatchPoints } from "./CatchPoints"
+import { Button, ButtonProps } from "./Button"
+import { CatchPointProps, CatchPoints } from "./CatchPoints"
 import { Typography } from "./Typography"
 
-export function Landing() {
+type LandingProps = {
+    readonly title: string
+    readonly subtitle: string
+    readonly text: React.ReactNode
+    readonly catchPoints: readonly CatchPointProps[]
+    readonly buttonText: string
+    readonly buttonProps?: ButtonProps
+}
+
+export function Landing({ title, subtitle, text, catchPoints, buttonText }: LandingProps) {
     return (
         <header className='flex flex-row flex-wrap items-center justify-center px-5'>
             <div>
                 <span id='home'>&nbsp;</span>
                 <Typography variant='h2' component='h1'>
-                    Nauč se programovat
+                    {title}
                 </Typography>
                 <Typography variant='h1' component='h1' className='uppercase'>
-                    a nech si za to platit!
+                    {subtitle}
                 </Typography>
                 <Typography component='p' className='max-w-xsProse'>
-                    Klademe si za cíl pomoci komukoliv získat dovednosti a{"\u00A0"}znalosti vhodné pro práci v IT. To
-                    vše bez časových limitací a za cenu dostupnou pro každého.
+                    {text}
                 </Typography>
                 <div className='mt-20 grid grid-rows-4 md:grid-cols-2 md:grid-rows-2'>
-                    <CatchPoints icon={<LearnEarn />}>Učíš se a zároveň vyděláváš</CatchPoints>
-                    <CatchPoints icon={<PayConsultancy />}>
-                        Platíš pouze za konzultace s{"\u00A0"}odborníkem z oboru
-                    </CatchPoints>
-                    <CatchPoints icon={<Time />}>
-                        Rychlost kurzu si{"\u00A0"}určuješ{"\u00A0"}sám
-                    </CatchPoints>
-                    <CatchPoints icon={<Worldwide />}>Celý kurz je online přístupný odkudkoliv</CatchPoints>
+                    {catchPoints.map(({ icon, children }) => (
+                        <CatchPoints icon={icon}>{children}</CatchPoints>
+                    ))}
                 </div>
                 <Button theme='off' size='large' className='mx-auto mb-10 xl:mb-0 xl:mt-20 block xl:mx-0'>
-                    Vyzkoušej ZDARMA
+                    {buttonText}
                 </Button>
             </div>
             <img
