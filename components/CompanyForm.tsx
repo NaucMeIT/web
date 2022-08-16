434109ddc69a833e7ad9b7d5d3518a8d4b7e8bf9
Petr Glaser <syreanis+1@gmail.com>
2022-08-17 00:37:18 +0200

    feat(fomr): extract logic to form wrapper

--- components/CompanyForm.tsx
+++ components/CompanyForm.tsx
@@ -1,42 +1,9 @@
-import { Form, useFormSubmit } from "next-runtime/form"
-import { Button } from "./Button"
 import { DecoratedInput } from "./DecoratedInput"
-import { Typography } from "./Typography"
-import { EmailLink } from "./EmailLink"
-import { useRecaptcha } from "../hooks/useRecaptcha"
-import { EmailThanks } from "./EmailThanks"
+import { FormWrapper } from "./FormWrapper"
 
 export function CompanyForm() {
-    const { isSubmitting, isSuccess, isError } = useFormSubmit()
-    const token = useRecaptcha()
-
-    if (isSuccess) {
-        return <EmailThanks />
-    }
-
     return (
-        <Form className='mx-auto flex w-10/12 flex-col gap-y-4 md:w-6/12 mt-36' method='post'>
-            <span id='contact'>&nbsp;</span>
-            <Typography variant='h2' component='h2' className='mx-auto mb-4'>
-                Napište nám
-            </Typography>
-            {isError && (
-                <Typography
-                    component='p'
-                    variant='error'
-                    className='mx-auto max-w-prose text-center'
-                    componentProps={{ role: "alert" }}
-                >
-                    Email se nepodařilo odeslat, zkuste to prosím znovu. Případně nám neváhejte zavolat či napsat na
-                    email: <EmailLink email='info@naucme.it' />
-                </Typography>
-            )}
-            {!isError && (
-                <Typography component='p' className='mx-auto max-w-prose text-center'>
-                    Máte zájem o zaměstnance? Rádi byste si domluvili schůzku a zjistili podrobnosti? Zanechte nám na
-                    sebe kontakt případně napište na email: <EmailLink email='info@naucme.it' />
-                </Typography>
-            )}
+        <FormWrapper text='Máte zájem o zaměstnance? Rádi byste si domluvili schůzku a zjistili podrobnosti? Zanechte nám na sebe kontakt případně napište na email:'>
             <DecoratedInput name='name' type='text' label='Vaše jméno' placeholder='Zadejte své jméno' required />
             <DecoratedInput name='company' type='text' label='Firma' placeholder='Zadejte jméno firmy' required />
             <div className='flex flex-col gap-9 md:flex-row'>
@@ -71,15 +38,6 @@ export function CompanyForm() {
                 required
                 className='resize-vertical min-h-28'
             />
-            <input type='hidden' id='recaptcha' name='recaptcha' value={token} />
-            <Typography variant='form' className='text-center'>
-                Tato stránka je chráněna reCAPTCHA, platí
-                <a href='https://policies.google.com/privacy'> zásady ochrany osobních údajů</a> a
-                <a href='https://policies.google.com/terms'> smluvní podmínky</a> společnosti Google.
-            </Typography>
-            <Button size='large' type='submit' theme='off' className='w-fit self-end' disabled={isSubmitting}>
-                {isSubmitting ? "Odesílám" : "Odeslat zprávu"}
-            </Button>
-        </Form>
+        </FormWrapper>
     )
 }
