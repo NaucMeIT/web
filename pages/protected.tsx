182c6345091c7c7194bac77b5953e3bf3b003429
Petr Glaser <syreanis+1@gmail.com>
2022-09-22 18:17:01 +0200

    feat(login): add protected page + types

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -1,7 +1,29 @@
-import type { NextPage } from "next"
+import type { GetServerSideProps, NextPage } from "next"
+import { authOptions } from "./api/auth/[...nextauth]"
+import { unstable_getServerSession } from "next-auth/next"
+import { Session } from "next-auth"
 
-const Login: NextPage = () => {
-    return <div>Protected</div>
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const session = await unstable_getServerSession(context.req, context.res, authOptions)
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        }
+    }
+
+    return {
+        props: {
+            session,
+        },
+    }
+}
+
+const Login: NextPage<{ readonly session: Session }> = ({ session }) => {
+    return <div>Protected - {JSON.stringify(session)}</div>
 }
 
 export default Login
