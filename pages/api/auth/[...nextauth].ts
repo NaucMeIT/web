df0d1765c24be729e0e25fd2bb48bbc38431e0f3
Petr Glaser <syreanis+1@gmail.com>
2022-09-28 23:19:12 +0200

    feat(login): add facebook login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import NextAuth from "next-auth"
+import FacebookProvider from "next-auth/providers/facebook"
 import GoogleProvider from "next-auth/providers/google"
 import EmailProvider from "next-auth/providers/email"
 // import nodemailer from "nodemailer"
@@ -32,6 +33,10 @@ export const authOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_ID || "",
             clientSecret: process.env.GOOGLE_SECRET || "",
+        }),
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_CLIENT_ID || "",
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET || "",
         }),
     ],
     adapter: PrismaAdapter(prisma),
