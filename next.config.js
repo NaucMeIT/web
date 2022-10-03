db0686ab5409f1574534bfd636429a2ec4957eae
Petr Glaser <syreanis+1@gmail.com>
2022-10-04 02:06:02 +0300

    feat(pp-tos): add privacy policy and terms of service

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withMDX = require("@next/mdx")({
     options: {
         remarkPlugins: [],
         rehypePlugins: [],
-        // providerImportSource: "@mdx-js/react",
+        providerImportSource: "@mdx-js/react",
     },
 })
 
