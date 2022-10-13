d66487e44a1a5f9882af130bb7f1759408da870d
Petr Glaser <syreanis+1@gmail.com>
2022-10-13 21:21:27 +0300

    refactor(images): use next/future/image + optimise svg dangerously

--- components/CourseBox.tsx
+++ components/CourseBox.tsx
@@ -1,22 +1,26 @@
+import Image from "next/future/image"
 import Link from "next/link"
 import * as React from "react"
 import { Typography } from "./Typography"
+
 type Props = {
     readonly image: string
+    readonly width: number
+    readonly height: number
     readonly side: "right" | "left"
     readonly title: string
     readonly children: React.ReactNode
     readonly link?: any
 }
 
-export function CourseBox({ image, side, title, children, link }: Props) {
+export function CourseBox({ image, width, height, side, title, children, link }: Props) {
     return (
         <div
             className={`flex flex-col ${
                 side === "left" ? "md:flex-row" : "md:flex-row-reverse"
             } items-center justify-center gap-x-36 gap-y-4 px-5`}
         >
-            <img src={image} width={320} height={348} alt={`Ilustrace ke kurzu ${title}`} loading='lazy' />
+            <Image src={image} width={width} height={height} alt={`Ilustrace ke kurzu ${title}`} loading='lazy' />
             <div className='flex w-fit max-w-lg flex-col'>
                 <Typography variant='h3' component='h3' className='mb-8'>
                     {title}
