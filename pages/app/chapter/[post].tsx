986b427eb301a2cc25bba7b243afbe45be390c8e
Petr Glaser <syreanis+1@gmail.com>
2022-10-20 11:29:52 +0300

    feat(mdx): chapter sidemenu + logo link update

--- pages/app/chapter/[post].tsx
+++ pages/app/chapter/[post].tsx
@@ -1,5 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from "next"
-import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote"
+import { MDXRemote, MDXRemoteProps } from "next-mdx-remote"
 import { serialize } from "next-mdx-remote/serialize"
 import path from "path"
 import { getAndParseMdx, getDataFromParsedMdx, getFilesAt } from "../../../utils/mdx"
@@ -20,32 +20,33 @@ type MenuData = Record<
         }[]
         readonly content: string
         readonly data: {
-            readonly [key: string]: any
+            readonly title: string
         }
     }
 >
 
 type PostProps = {
-    readonly mdx: MDXRemoteSerializeResult<Record<string, unknown>, Record<string, string>>
+    readonly mdx: MDXRemoteProps
     readonly menuData: MenuData
     readonly metaInformation: Record<string, string>
 }
 
 const Post: React.FC<PostProps> = ({ mdx, metaInformation, menuData }) => {
     const headings = Object.entries(menuData).flatMap(([_, d]) => d.headings)
+
     return (
         <>
             <Head desc={metaInformation.abstract} url=''>
                 <title>{metaInformation.title}</title>
             </Head>
-            <Menu items={[]} />
+            <Menu items={[]} logoLink='#' />
             <div className='grid grid-cols-12 auto-rows-auto h-screen'>
-                <div className='row-end-7 row-span-full col-span-2 col-start-1 mt-20 bg-secondary/5 overflow-hidden'>
+                <div className='row-start-1 row-end-2 xl:row-end-7 xl:row-span-full col-span-full xl:col-span-2 mt-20 bg-secondary/5 overflow-auto'>
                     <SideMenu>
                         <TableOfContents headings={headings} />
                     </SideMenu>
                 </div>
-                <main className='row-end-7 col-start-3 col-span-full row-span-full overflow-auto px-10 mt-20 pb-2 overscroll-none'>
+                <main className='row-end-7 xl:col-start-3 col-span-full row-start-3 xl:row-start-1 row-span-full overflow-auto px-10 xl:mt-20 pb-2 overscroll-none'>
                     <Typography className='py-4' variant='h2' component='h1'>
                         {metaInformation.title}
                     </Typography>
