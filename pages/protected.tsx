e636f44704fcfa4302495fa3ed1a51c9670d785c
Petr Glaser <syreanis+1@gmail.com>
2022-10-21 01:51:09 +0300

    fix(tracking): fix prisma import on protected

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -1,12 +1,13 @@
 import type { GetServerSideProps, NextPage } from "next"
-import { authOptions } from "./api/auth/[...nextauth]"
 import { unstable_getServerSession } from "next-auth/next"
 import { Session } from "next-auth"
-import { Button } from "../components/Button"
 import { signOut } from "next-auth/react"
 import splitbee from "@splitbee/web"
 import { useEffect } from "react"
 import { Plan } from "@prisma/client"
+import { authOptions } from "./api/auth/[...nextauth]"
+import { prisma } from "../utils/prisma"
+import { Button } from "../components/Button"
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const session = await unstable_getServerSession(context.req, context.res, authOptions)
