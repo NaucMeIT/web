a4a23359036df14d76152376c21fd93b958b761e
Petr Glaser <syreanis+1@gmail.com>
2022-10-25 15:04:48 +0300

    chore(deps): use next canary to prepared for next conf

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,4 +1,4 @@
-import Image from "next/future/image"
+import Image from "next/image"
 import { Button, ButtonProps } from "./Button"
 import { CatchPointProps, CatchPoints } from "./CatchPoints"
 import { Typography } from "./Typography"
