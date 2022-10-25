68d9772880d26f38170ce047c73ede769493c652
Petr Glaser <syreanis+1@gmail.com>
2022-10-26 01:19:22 +0300

    feat(report): add chapter name to error report

--- components/ErrorForm.tsx
+++ components/ErrorForm.tsx
@@ -1,6 +1,7 @@
 import { Session } from "next-auth"
 import { DecoratedInput } from "./DecoratedInput"
 import { FormWrapper } from "./FormWrapper"
+import { useRouter } from "next/router"
 
 // eslint-disable-next-line functional/no-mixed-type
 type Props = {
@@ -10,8 +11,11 @@ type Props = {
 }
 
 export function ErrorForm({ user, onSuccess }: Props) {
+    const router = useRouter()
+
     return (
         <FormWrapper onSuccess={onSuccess} type='error' text='Našli jste chybu? Neváhejte nám ji nahlásit!'>
+            <input type='hidden' id='chapter' name='chapter' value={router.query.post || ""} />
             <input type='hidden' id='name' name='name' value={user?.name || ""} />
             <input type='hidden' id='email' name='email' value={user?.email || ""} />
             <DecoratedInput
@@ -19,7 +23,7 @@ export function ErrorForm({ user, onSuccess }: Props) {
                 component='textarea'
                 rows={10}
                 label='Nalezená chyba'
-                placeholder='Popiště chybu, kterou jste nalezli...'
+                placeholder='Popište chybu, kterou jste nalezli...'
                 required
                 className='resize-vertical min-h-28'
             />
