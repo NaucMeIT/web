4150e667ab9e32b3e5d2c34775f7b18ae43ce047
Petr Glaser <syreanis+1@gmail.com>
2022-10-28 17:38:15 +0300

    fix(chapter): remove dashboard from sidemenu

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -5,10 +5,7 @@ type Props = {
 export function SideMenu({ children }: Props) {
     return (
         <nav className='h-full flex flex-col justify-between bg-rightSide bg-no-repeat bg-contain bg-rightCut pl-4 pr-20 py-4'>
-            <div>
-                Dashboard
-                {children}
-            </div>
+            <div>{children}</div>
         </nav>
     )
 }
