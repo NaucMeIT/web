d509cce9de87c8dc4fd4f42464cd60ecebe3c2d7
Petr Glaser <syreanis+1@gmail.com>
2022-10-28 21:29:35 +0300

    chore(deps): update next + its packages to latest

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,7 @@ export default withMDX(
         },
         experimental: {
             newNextLinkBehavior: true,
+            appDir: true,
         },
         images: {
             formats: ["image/avif", "image/webp"],
