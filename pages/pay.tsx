1b0831a1a0b19d1847d16aa455a07a9e2e922ac9
Petr Glaser <syreanis+1@gmail.com>
2022-10-30 00:54:42 +0300

    feat(stripe): fetch selected plan + show its data

--- pages/pay.tsx
+++ pages/pay.tsx
@@ -1,30 +1,73 @@
 import React from "react"
 import { loadStripe } from "@stripe/stripe-js"
 import { Elements } from "@stripe/react-stripe-js"
-
+import createStripe from "stripe"
 import CheckoutForm from "../components/CheckoutForm"
 import { NextPage } from "next"
 import { Typography } from "../components/Typography"
+import type { GetServerSideProps } from "next"
+import { Plan } from "@prisma/client"
+import { prisma } from "../utils/prisma"
+import { unstable_getServerSession } from "next-auth"
+import { authOptions } from "./api/auth/[...nextauth]"
 
-// Make sure to call loadStripe outside of a component’s render to avoid
-// recreating the Stripe object on every render.
-// This is your test publishable API key.
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || "")
 
-const Pay: NextPage = () => {
-    const [clientSecret, setClientSecret] = React.useState("")
+const getPlan = async (planId: string) => {
+    const plan = await prisma.plan.findFirst({
+        where: {
+            id: planId,
+        },
+    })
 
-    React.useEffect(() => {
-        // Create PaymentIntent as soon as the page loads
-        fetch("/api/create-payment-intent", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ planId: "cl9ac83yn000509mh606b3p8i" }),
-        })
-            .then((res) => res.json())
-            .then((data) => setClientSecret(data.clientSecret))
-    }, [])
+    return plan
+}
 
+const calculatePlanPrice = async (plan: Plan) => {
+    return plan.price
+}
+
+const registerRedirect = {
+    redirect: {
+        destination: "/register",
+        statusCode: 302,
+    },
+} as const
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const stripe = new createStripe(process.env.STRIPE_SECRET_KEY || "", { apiVersion: "2022-08-01", typescript: true })
+    const session = await unstable_getServerSession(context.req, context.res, authOptions)
+    const planId = session?.user.planId
+
+    if (!planId) {
+        return registerRedirect
+    }
+
+    const plan = await getPlan(planId)
+
+    if (!plan) {
+        return registerRedirect
+    }
+
+    const amount = await calculatePlanPrice(plan)
+
+    const { client_secret: clientSecret } = await stripe.paymentIntents.create({
+        amount,
+        currency: "czk",
+        automatic_payment_methods: {
+            enabled: true,
+        },
+    })
+
+    return {
+        props: {
+            clientSecret,
+            plan,
+        },
+    }
+}
+
+const Pay: NextPage<{ readonly plan: Plan; readonly clientSecret: string }> = ({ plan, clientSecret }) => {
     const appearance = {
         theme: "stripe",
         labels: "floating",
@@ -50,18 +93,16 @@ const Pay: NextPage = () => {
 
     return (
         <div className='w-full max-w-lg mx-auto px-4 min-h-screen flex flex-col justify-center'>
+            <Typography variant='h3' className='mb-4 text-center'>
+                Vybrán plán {plan.name}
+            </Typography>
+            <Typography className='mb-6 text-center'>
+                Cena pro vás je {plan.price} Kč. Detaily plánu zjistíte na svém profilu.
+            </Typography>
             {clientSecret && (
-                <>
-                    <Typography variant='h3' className='mb-4 text-center'>
-                        Vybrán plán X
-                    </Typography>
-                    <Typography className='mb-6 text-center'>
-                        Cena pro vás je YYYY Kč. Detaily plánu zjistíte na svém profilu.
-                    </Typography>
-                    <Elements options={options} stripe={stripePromise}>
-                        <CheckoutForm />
-                    </Elements>
-                </>
+                <Elements options={options} stripe={stripePromise}>
+                    <CheckoutForm />
+                </Elements>
             )}
         </div>
     )
