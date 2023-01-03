a28c634f9e87a69d2beb70e3b491c44dfa51412f
Petr Glaser <syreanis+1@gmail.com>
2023-01-03 23:42:08 +0000

    fix(spoiler): fix button position in container

--- components/Spoiler.tsx
+++ components/Spoiler.tsx
@@ -10,21 +10,17 @@ export function Spoiler({ children, textButton }: Props) {
     const [showHiddenText, setShowHiddenText] = useState(true)
 
     return (
-        <>
-            <div className='z-10 absolute my-14 ml-4 sm:ml-8 md:ml-12 lg:ml-20 xl:ml-32 2xl:ml-64'>
-                {showHiddenText && (
-                    <Button theme='off' onClick={() => setShowHiddenText(false)}>
+        <div className='relative'>
+            {showHiddenText && (
+                <div className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2'>
+                    <Button className='z-10' theme='off' onClick={() => setShowHiddenText(false)}>
                         {textButton}
                     </Button>
-                )}
-            </div>
-            <div
-                className={`w-1/3 m-4 cursor-not-allowed pointer-events-none select-none ${
-                    showHiddenText && "blur-sm"
-                }`}
-            >
+                </div>
+            )}
+            <div className={`m-4 cursor-not-allowed pointer-events-none select-none ${showHiddenText && "blur-sm"}`}>
                 {children}
             </div>
-        </>
+        </div>
     )
 }
