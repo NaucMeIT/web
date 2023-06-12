2ce3c05e8606157c81ed39a4ae20a9cf5997cf50
Petr Glaser <syreanis+1@gmail.com>
2023-06-12 08:48:58 +0200

    fix(arrow): make arrow always scroll

--- components/DownArrow.tsx
+++ components/DownArrow.tsx
@@ -2,7 +2,14 @@ import type { SVGProps } from "react"
 
 export function DownArrow(props: Readonly<SVGProps<SVGSVGElement>>) {
     return (
-        <svg xmlns='http://www.w3.org/2000/svg' width={34.453} height={45.507} viewBox='0 0 34.453 45.507' {...props}>
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width={34.453}
+            height={45.507}
+            viewBox='0 0 34.453 45.507'
+            className='mx-auto mb-20 cursor-pointer hidden md:block'
+            onClick={() => window.scrollBy(0, document.documentElement.clientHeight * 0.8)}
+        >
             <g data-name='Component 14 \u2013 1' fill='none' stroke='#fff' strokeWidth={1.5}>
                 <path
                     className='motion-safe:animate-pulseDown'
