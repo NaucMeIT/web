92dd8b9405667fec52e355f1eed78d9ce205b1c4
Petr Glaser <syreanis+1@gmail.com>
2023-08-28 14:15:56 +0200

    feat(builder): add packages to Builder.io

--- components/Packages.tsx
+++ components/Packages.tsx
@@ -1,16 +1,39 @@
-import { packages } from "../utils/packages"
+import { packages as defaultPackages, packages } from "../utils/packages"
 import { PackageBox } from "./PackageBox"
 import { Typography } from "./Typography"
 
-export function Packages() {
+type PackagesProps = {
+    readonly packages?: typeof defaultPackages
+    readonly headline?: string
+    readonly subHeadline?: string
+}
+
+type BuilderPackage = {
+    readonly title: string
+    readonly benefits: readonly { text: string }[]
+    readonly price: number
+    readonly priority: boolean
+}
+
+type BuilderPackagesProps = {
+    readonly packages: BuilderPackage[]
+    readonly headline: string
+    readonly subHeadline: string
+}
+
+export function Packages({
+    packages = defaultPackages,
+    headline = "Začni se učit ještě dnes!",
+    subHeadline = "Vyber si jeden z našich výhodných balíčků a ušetři pro začátek pár kaček.",
+}: PackagesProps) {
     return (
         <section className='flex flex-col items-center justify-center gap-4 bg-alt px-5 pt-20 pb-28'>
             <span id='packages'>&nbsp;</span>
             <Typography variant='h2' component='h2' className='mb-8 text-center'>
-                Začni se učit ještě dnes!
+                {headline}
             </Typography>
             <Typography className='mb-6 text-center' component='p'>
-                Vyber si jeden z našich výhodných balíčků a ušetři pro začátek pár kaček.
+                {subHeadline}
             </Typography>
             <div className='grid md:grid-cols-2 md:grid-rows-2 xl:grid-cols-4 xl:grid-rows-1 justify-center gap-8'>
                 {packages.map((p) => (
@@ -20,6 +43,27 @@ export function Packages() {
                         buttonProps={{ href: `/sign?startPlan=${p.title}` }}
                         priority={p.title === "Standard"}
                     />
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export function BuilderPackages({ packages, headline, subHeadline }: BuilderPackagesProps) {
+    const mappedPackages = packages?.map((p) => ({ ...p, benefits: p.benefits.map((b) => b.text) }))
+
+    return (
+        <section className='flex flex-col items-center justify-center gap-4 bg-alt px-5 pt-20 pb-28'>
+            <span id='packages'>&nbsp;</span>
+            <Typography variant='h2' component='h2' className='mb-8 text-center'>
+                {headline}
+            </Typography>
+            <Typography className='mb-6 text-center' component='p'>
+                {subHeadline}
+            </Typography>
+            <div className='grid md:grid-cols-2 md:grid-rows-1 xl:grid-cols-1 xl:grid-rows-1 justify-center gap-8'>
+                {mappedPackages.map((p) => (
+                    <PackageBox key={p.title} {...p} buttonProps={{ href: "#contact" }} priority={p.priority} />
                 ))}
             </div>
         </section>
