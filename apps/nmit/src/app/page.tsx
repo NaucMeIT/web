b62f142b69b72ceaa358c5cd8e3a9f483c1a083c
Petr Glaser <syreanis+1@gmail.com>
2024-06-06 13:43:13 +0200

    feat(quiz): add document parse route

--- apps/nmit/src/app/page.tsx
+++ apps/nmit/src/app/page.tsx
@@ -1,7 +1,12 @@
+const acceptedFileTypes =
+  'application/pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/vnd.ms-powerpoint,application/vnd.openxmlformats-officedocument.presentationml.presentation,application/vnd.oasis.opendocument.text,application/vnd.oasis.opendocument.presentation,text/html,image/x-png,image/jpeg,image/gif'
 export default function Index() {
   return (
     <div>
-      <h1 className='text-2xl bg-destructive'>Welcome to nmit!</h1>
+      <form action='/api/quiz' method='POST' encType='multipart/form-data'>
+        <input type='file' id='file' name='file' accept={acceptedFileTypes} />
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
