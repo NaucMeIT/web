bc7893875f1ff7f74af6af24a679437f932ae01e
Petr Glaser <syreanis+1@gmail.com>
2024-06-20 00:38:51 +0200

    feat: add simplified file upload to google + ai

---
+++ libs/ai/src/google.ts
@@ -0,0 +1,268 @@
+import {
+  FileMetadata,
+  FileMetadataResponse,
+  ListFilesResponse,
+  ListParams,
+  UploadFileResponse,
+  GoogleGenerativeAIError,
+  GoogleGenerativeAIRequestInputError,
+  GoogleGenerativeAIFetchError,
+} from './google-types'
+import { RequestOptions } from '@google/generative-ai'
+
+const DEFAULT_BASE_URL = 'https://generativelanguage.googleapis.com'
+const DEFAULT_API_VERSION = 'v1beta'
+const PACKAGE_VERSION = '__PACKAGE_VERSION__'
+const PACKAGE_LOG_HEADER = 'genai-js'
+
+function getClientHeaders(requestOptions: RequestOptions): string {
+  const clientHeaders = []
+  if (requestOptions?.apiClient) {
+    clientHeaders.push(requestOptions.apiClient)
+  }
+  clientHeaders.push(`${PACKAGE_LOG_HEADER}/${PACKAGE_VERSION}`)
+  return clientHeaders.join(' ')
+}
+
+enum FilesTask {
+  UPLOAD = 'upload',
+  LIST = 'list',
+  GET = 'get',
+  DELETE = 'delete',
+}
+
+const taskToMethod = {
+  [FilesTask.UPLOAD]: 'POST',
+  [FilesTask.LIST]: 'GET',
+  [FilesTask.GET]: 'GET',
+  [FilesTask.DELETE]: 'DELETE',
+}
+
+class FilesRequestUrl {
+  private _url: URL
+
+  constructor(
+    public task: FilesTask,
+    public apiKey: string,
+    public requestOptions?: RequestOptions,
+  ) {
+    const apiVersion = this.requestOptions?.apiVersion || DEFAULT_API_VERSION
+    const baseUrl = this.requestOptions?.baseUrl || DEFAULT_BASE_URL
+    let initialUrl = baseUrl
+    if (this.task === FilesTask.UPLOAD) {
+      initialUrl += `/upload`
+    }
+    initialUrl += `/${apiVersion}/files`
+    this._url = new URL(initialUrl)
+  }
+
+  appendPath(path: string): void {
+    this._url.pathname = this._url.pathname + `/${path}`
+  }
+
+  appendParam(key: string, value: string): void {
+    this._url.searchParams.append(key, value)
+  }
+
+  toString(): string {
+    return this._url.toString()
+  }
+}
+
+function getHeaders(url: FilesRequestUrl): Headers {
+  const headers = new Headers()
+  headers.append('x-goog-api-client', getClientHeaders(url.requestOptions as any))
+  headers.append('x-goog-api-key', url.apiKey)
+  return headers
+}
+
+async function makeFilesRequest(
+  url: FilesRequestUrl,
+  headers: Headers,
+  body?: Blob,
+  fetchFn: typeof fetch = fetch,
+): Promise<Response> {
+  const requestInit: RequestInit = {
+    method: taskToMethod[url.task],
+    headers,
+  }
+
+  if (body) {
+    requestInit.body = body
+  }
+
+  const signal = getSignal(url.requestOptions)
+  if (signal) {
+    requestInit.signal = signal
+  }
+
+  try {
+    console.log(url.toString())
+    console.log(requestInit)
+    const response = await fetchFn(url.toString(), requestInit)
+    if (!response.ok) {
+      let message = ''
+      let errorDetails
+      try {
+        const json = await response.json()
+        message = json.error.message
+        if (json.error.details) {
+          message += ` ${JSON.stringify(json.error.details)}`
+          errorDetails = json.error.details
+        }
+      } catch (e) {
+        // ignored
+      }
+      throw new GoogleGenerativeAIFetchError(
+        `Error fetching from ${url.toString()}: [${response.status} ${response.statusText}] ${message}`,
+        response.status,
+        response.statusText,
+        errorDetails,
+      )
+    } else {
+      return response
+    }
+  } catch (e) {
+    let err: any = e
+    if (!(e instanceof GoogleGenerativeAIFetchError)) {
+      err = new GoogleGenerativeAIError(`Error fetching from ${url.toString()}: ${(e as any).message}`)
+      err.stack = (e as any).stack
+    }
+    throw err
+  }
+}
+
+/**
+ * Get AbortSignal if timeout is specified
+ */
+function getSignal(requestOptions?: RequestOptions): AbortSignal | null | undefined {
+  if (requestOptions?.timeout && requestOptions?.timeout >= 0) {
+    const abortController = new AbortController()
+    const signal = abortController.signal
+    setTimeout(() => abortController.abort(), requestOptions.timeout)
+    return signal
+  }
+}
+
+/**
+ * Class for managing GoogleAI file uploads.
+ * @public
+ */
+export class GoogleAIFileManager {
+  constructor(
+    public apiKey: string,
+    private _requestOptions?: RequestOptions,
+  ) {}
+
+  /**
+   * Upload a file
+   * Altered version which accepts file directly, not just file path
+   * This is fork of official Google Generative AI SDK
+   */
+  async uploadFile(file: File, fileMetadata: FileMetadata): Promise<UploadFileResponse> {
+    const url = new FilesRequestUrl(FilesTask.UPLOAD, this.apiKey, this._requestOptions)
+
+    const uploadHeaders = getHeaders(url)
+    const boundary = generateBoundary()
+    uploadHeaders.append('X-Goog-Upload-Protocol', 'multipart')
+    uploadHeaders.append('Content-Type', `multipart/related; boundary=${boundary}`)
+
+    const uploadMetadata = getUploadMetadata(fileMetadata)
+
+    // Multipart formatting code taken from @firebase/storage
+    const metadataString = JSON.stringify({ file: uploadMetadata })
+    const preBlobPart =
+      '--' +
+      boundary +
+      '\r\n' +
+      'Content-Type: application/json; charset=utf-8\r\n\r\n' +
+      metadataString +
+      '\r\n--' +
+      boundary +
+      '\r\n' +
+      'Content-Type: ' +
+      fileMetadata.mimeType +
+      '\r\n\r\n'
+    const postBlobPart = '\r\n--' + boundary + '--'
+    const blob = new Blob([preBlobPart, file, postBlobPart])
+
+    const response = await makeFilesRequest(url, uploadHeaders, blob)
+    return response.json()
+  }
+
+  /**
+   * List all uploaded files
+   */
+  async listFiles(listParams?: ListParams): Promise<ListFilesResponse> {
+    const url = new FilesRequestUrl(FilesTask.LIST, this.apiKey, this._requestOptions)
+    if (listParams?.pageSize) {
+      url.appendParam('pageSize', listParams.pageSize.toString())
+    }
+    if (listParams?.pageToken) {
+      url.appendParam('pageToken', listParams.pageToken)
+    }
+    const uploadHeaders = getHeaders(url)
+    const response = await makeFilesRequest(url, uploadHeaders)
+    return response.json()
+  }
+
+  /**
+   * Get metadata for file with given ID
+   */
+  async getFile(fileId: string): Promise<FileMetadataResponse> {
+    const url = new FilesRequestUrl(FilesTask.GET, this.apiKey, this._requestOptions)
+    url.appendPath(parseFileId(fileId))
+    const uploadHeaders = getHeaders(url)
+    const response = await makeFilesRequest(url, uploadHeaders)
+    return response.json()
+  }
+
+  /**
+   * Delete file with given ID
+   */
+  async deleteFile(fileId: string): Promise<void> {
+    const url = new FilesRequestUrl(FilesTask.DELETE, this.apiKey, this._requestOptions)
+    url.appendPath(parseFileId(fileId))
+    const uploadHeaders = getHeaders(url)
+    await makeFilesRequest(url, uploadHeaders)
+  }
+}
+
+/**
+ * If fileId is prepended with "files/", remove prefix
+ */
+function parseFileId(fileId: string): string {
+  if (fileId.startsWith('files/')) {
+    return fileId.split('files/')[1]
+  }
+  if (!fileId) {
+    throw new GoogleGenerativeAIError(
+      `Invalid fileId ${fileId}. ` + `Must be in the format "files/filename" or "filename"`,
+    )
+  }
+
+  return fileId
+}
+
+function generateBoundary(): string {
+  let str = ''
+  for (let i = 0; i < 2; i++) {
+    str = str + Math.random().toString().slice(2)
+  }
+  return str
+}
+
+function getUploadMetadata(inputMetadata: FileMetadata): FileMetadata {
+  if (!inputMetadata.mimeType) {
+    throw new GoogleGenerativeAIRequestInputError('Must provide a mimeType.')
+  }
+  const uploadMetadata: FileMetadata = {
+    mimeType: inputMetadata.mimeType,
+    displayName: inputMetadata.displayName,
+  }
+
+  if (inputMetadata.name) {
+    uploadMetadata.name = inputMetadata.name.includes('/') ? inputMetadata.name : `files/${inputMetadata.name}`
+  }
+  return uploadMetadata
+}
--- libs/ai/src/media.ts
+++ libs/ai/src/media.ts
@@ -1,167 +1,79 @@
 'use server'
 
-import { generateText } from 'ai'
-import { google } from '@ai-sdk/google'
-import { Storage } from '@google-cloud/storage'
+import { GoogleGenerativeAI } from '@google/generative-ai'
 import { trim } from '@nmit-coursition/utils'
+import { GoogleAIFileManager } from './google'
 
-// TODO: Patch the AI SDK, because it allows only inline content (base64) and not file URLs
-// https://github.com/vercel/ai/blob/main/packages/google/src/convert-to-google-generative-ai-messages.ts
-export async function getTranscript(fileUrl: string) {
-  const { text } = await generateText({
-    model: google('models/gemini-1.5-flash-latest'),
-    messages: [
-      {
-        role: 'system',
-        content: trim`
-          You are an expert in making video/audio transcription in .srt format.
+const systemInstruction = trim`
+  You are an expert in transcription in .srt format.
 
-          EXAMPLE OUTPUT1:
-          1
-          00:00:00,498 --> 00:00:02,827
-          - Here's what I love most
-          about food and diet.
+  # Examples
+  ## Output1
+  1
+  00:00:00,498 --> 00:00:02,827
+  - Here's what I love most
+  about food and diet.
 
-          2
-          00:00:02,827 --> 00:00:06,383
-          We all eat several times a day,
-          and we're totally in charge
+  2
+  00:00:02,827 --> 00:00:06,383
+  We all eat several times a day,
+  and we're totally in charge
 
-          3
-          00:00:06,383 --> 00:00:09,427
-          of what goes on our plate
-          and what stays off.
+  3
+  00:00:06,383 --> 00:00:09,427
+  of what goes on our plate
+  and what stays off.
 
-          EXAMPLE OUTPUT2:
-          1
-          00:00:03,400 --> 00:00:06,177
-          In this lesson, we're going to
-          be talking about finance. And
+  ## Output2
+  1
+  00:00:03,400 --> 00:00:06,177
+  In this lesson, we're going to
+  be talking about finance. And
 
-          2
-          00:00:06,177 --> 00:00:10,009
-          one of the most important aspects
-          of finance is interest.
+  2
+  00:00:06,177 --> 00:00:10,009
+  one of the most important aspects
+  of finance is interest.
 
-          3
-          00:00:10,009 --> 00:00:13,655
-          When I go to a bank or some
-          other lending institution
+  3
+  00:00:10,009 --> 00:00:13,655
+  When I go to a bank or some
+  other lending institution
 
-          4
-          00:00:13,655 --> 00:00:17,720
-          to borrow money, the bank is happy
-          to give me that money. But then I'm
+  4
+  00:00:13,655 --> 00:00:17,720
+  to borrow money, the bank is happy
+  to give me that money. But then I'm
 
-          5
-          00:00:17,900 --> 00:00:21,480
-          going to be paying the bank for the
-          privilege of using their money.
-        `,
-      },
-      {
-        role: 'user',
-        content: [
-          {
-            type: 'text',
-            text: trim`
-              Can you transcribe this audio, in the format of timecode, caption?
-            `,
-          },
-          {
-            type: 'image',
-            image: new URL(fileUrl),
-          },
-        ],
-      },
-    ],
+  5
+  00:00:17,900 --> 00:00:21,480
+  going to be paying the bank for the
+  privilege of using their money.
+`
+
+export async function getTranscript(file: File) {
+  // Upload file
+  const fileManager = new GoogleAIFileManager(process.env['GOOGLE_GENERATIVE_AI_API_KEY'] || '')
+  const uploadResult = await fileManager.uploadFile(file, {
+    mimeType: 'video/mp4',
   })
+  const files = await fileManager.listFiles()
+  console.log(files)
 
-  return text
-}
-
-const bucketName = 'coursition-media-2024'
-const storage = new Storage({ keyFilename: 'coursition-storage.json' })
-
-export const uploadToGcs = async (file: File) => {
-  if (!file) throw new Error('No file provided')
-  if (file.size < 1) throw new Error('File is empty')
-
-  const buffer = await file.arrayBuffer()
-  const storage = new Storage()
-  await storage.bucket(bucketName).file(file.name).save(Buffer.from(buffer))
-
-  return true
-}
-
-export const uploadFile = async (form: FormData) => {
-  try {
-    const file = form.get('file') as File
-    await uploadToGcs(file)
-    return true
-  } catch (error) {
-    console.error(error)
-    return false
-  }
-}
-
-export const getSignedUrl = async (fileName: string) => {
-  const [url] = await storage
-    .bucket(bucketName)
-    .file(fileName)
-    .getSignedUrl({
-      action: 'write',
-      version: 'v4',
-      expires: Date.now() + 15 * 60 * 1000,
-      contentType: 'application/octet-stream',
-    })
-
-  return url
-}
-
-export async function deleteFile(fileName: string, deleteOptions = {}) {
-  try {
-    await storage.bucket(bucketName).file(fileName).delete(deleteOptions)
-    return true
-  } catch (error) {
-    console.error(error)
-    return false
-  }
-}
-
-export const setCors = async () => {
-  await storage.bucket(bucketName).setCorsConfiguration([
+  // Generate content
+  const genAI = new GoogleGenerativeAI(process.env['GOOGLE_GENERATIVE_AI_API_KEY'] || '')
+  const model = genAI.getGenerativeModel({
+    model: 'gemini-1.5-flash-latest',
+    systemInstruction,
+  })
+  const result = await model.generateContent([
     {
-      maxAgeSeconds: 3600,
-      method: ['GET', 'PUT'],
-      origin: ['*'],
-      responseHeader: ['Content-Type'],
+      fileData: {
+        mimeType: uploadResult.file.mimeType,
+        fileUri: uploadResult.file.uri,
+      },
     },
+    { text: 'Transcribe this video, in the format of timecode, caption in .srt format.' },
   ])
-}
-
-// TODO: Delete following content
-export const toBase64 = (file: Blob) =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onload = () => resolve(reader.result)
-    reader.onerror = reject
-  })
-
-export async function uploadMediaGoogleApis(file: File) {
-  const body = new FormData()
-  body.append('file', file)
-
-  const uploadResponse = await fetch(`https://generativelanguage.googleapis.com/upload/v1beta/files`, {
-    method: 'POST',
-    headers: {
-      accept: 'application/json',
-      contentType: 'multipart/form-data',
-      Authorization: `Bearer ${process.env['LLAMA_CLOUD_API_KEY']}`,
-    },
-    body,
-  })
-
-  return uploadResponse
+  return { result, files }
 }
