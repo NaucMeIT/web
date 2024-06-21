4d1b16f4c1ae1b6541b47fc9a171f5ab6bc3c5f7
Petr Glaser <syreanis+1@gmail.com>
2024-06-21 12:51:19 +0200

    refactor: :art: clean up the code and modularise it

--- libs/ui/design-system/src/lib/checkbox.tsx
+++ libs/ui/design-system/src/lib/checkbox.tsx
@@ -11,7 +11,7 @@ interface CheckboxProps {
 export function Checkbox({ id, label, subtext, disabled }: CheckboxProps) {
   return (
     <div className='items-top flex space-x-2'>
-      <CheckboxPrimitive id={id} disabled={disabled} />
+      <CheckboxPrimitive id={id} name={id} disabled={disabled} />
       <div className='grid gap-1.5 leading-none'>
         <Label htmlFor={id}>{label}</Label>
         {subtext && <p className='text-sm text-muted-foreground'>{subtext}</p>}
