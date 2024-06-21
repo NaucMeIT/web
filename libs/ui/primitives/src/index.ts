13a43dd2d6be90007605de0925ffcaaecab1afa7
Petr Glaser <syreanis+1@gmail.com>
2024-06-21 12:22:33 +0200

    feat(ui): :sparkles: add design system with necessary primitives

--- libs/ui/primitives/src/index.ts
+++ libs/ui/primitives/src/index.ts
@@ -1,1 +1,4 @@
+export * from './accordion'
 export * from './button'
+export * from './input'
+export * from './label'
--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -2,5 +2,4 @@ import * as zfd from './zodFormData'
 export * from './zodFormData'
 export { zfd }
 
-export * from './cn'
 export * from './string'
