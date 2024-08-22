91d8647ea1df1ecb2a1946694e5b10b8e4fb1f96
Petr Glaser <syreanis+1@gmail.com>
2024-08-22 07:38:51 +0200

    fix(legacy): :lipstick: fix banner on mobile

--- apps/legacy_nmit/components/SideMenu.tsx
+++ apps/legacy_nmit/components/SideMenu.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export function SideMenu({ children }: Props) {
     return (
-        <nav className='h-full flex flex-col justify-between bg-rightSide bg-no-repeat bg-contain bg-rightCut pl-4 pr-20 py-4'>
+        <nav className='h-full flex flex-col justify-between bg-rightSide bg-no-repeat bg-contain bg-rightCut pl-4 pr-20 py-4 pt-20'>
             <div>
                 <Typography
                     variant='normal'
