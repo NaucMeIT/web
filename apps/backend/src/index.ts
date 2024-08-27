a8f455e67cc77ea6edd180a069b45da02e2b09d9
Petr Glaser <syreanis+1@gmail.com>
2024-08-27 13:48:28 +0200

    feat(api): :tada: create ElysiaJS backend with one endpoint migrated

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,39 @@
-import { Elysia } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
+import { getTranscript } from '@nmit-coursition/ai'
+import { Elysia, t } from 'elysia'
 
 new Elysia()
-  .get('/', () => 'general kenobi')
-  .get('/hi', () => 'hi')
+  .use(swagger())
+  .group('/v1', (app) =>
+    app.group('/parse', (v1Api) =>
+      v1Api.post(
+        '/media',
+        async ({ body: { output, keywords, file } }) => {
+          const transcript = await getTranscript(file, keywords)
+
+          return {
+            ...(output.includes('vtt') ? { vtt: transcript.vtt } : {}),
+            ...(output.includes('srt') ? { srt: transcript.srt } : {}),
+            ...(output.includes('text') ? { text: transcript.raw } : {}),
+          }
+        },
+        {
+          body: t.Object({
+            file: t.File(),
+            output: t.Array(t.Union([t.Literal('vtt'), t.Literal('srt'), t.Literal('text')]), { default: ['text'] }),
+            keywords: t.Array(t.String(), { default: [] }),
+          }),
+          transform({ body }) {
+            body.output &&= Array.isArray(body.output) ? body.output : [body.output]
+            body.keywords &&= Array.isArray(body.keywords) ? body.keywords : [body.keywords]
+          },
+          response: t.Object({
+            vtt: t.Optional(t.String()),
+            srt: t.Optional(t.String()),
+            text: t.Optional(t.String()),
+          }),
+        },
+      ),
+    ),
+  )
   .listen(3000)
