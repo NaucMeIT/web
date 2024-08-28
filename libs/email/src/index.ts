39afedc14a324746d24a145f02a05efc00888f63
Petr Glaser <syreanis+1@gmail.com>
2024-08-28 23:10:46 +0200

    chore(deps): remove effect

--- libs/email/src/index.ts
+++ libs/email/src/index.ts
@@ -1,9 +1,5 @@
-import { Effect } from 'effect'
 import { type CreateEmailOptions, Resend } from 'resend'
 
 const resend = new Resend(process.env['RESEND_API_KEY'])
 
-export const send = (params: CreateEmailOptions) =>
-  Effect.gen(function* () {
-    return yield* Effect.tryPromise(async () => await resend.emails.send(params))
-  }).pipe(Effect.runPromise)
+export const send = async (params: CreateEmailOptions) => await resend.emails.send(params)
