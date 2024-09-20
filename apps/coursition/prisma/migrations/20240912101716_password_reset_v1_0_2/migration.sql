73ad475888b55538424a0fd1818ff0c1989e5571
Petr Glaser <syreanis+1@gmail.com>
2024-09-20 12:39:29 +0200

    refactor(db): :art: remove legacy prisma from coursition

--- apps/coursition/prisma/migrations/20240912101716_password_reset_v1_0_2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `email` to the `PasswordReset` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "PasswordReset" ADD COLUMN     "email" TEXT NOT NULL;
--- apps/coursition/prisma/migrations/20240912113153_password_reset_v1_0_4/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `expires_at` on the `PasswordReset` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "PasswordReset" DROP COLUMN "expires_at",
-ADD COLUMN     "expires_at" TIMESTAMP(3) NOT NULL;
