3447c326ab1828e5d62ba720bd5700cd8ff3530c
Petr Glaser <syreanis+1@gmail.com>
2024-09-23 15:25:22 +0200

    feat(ai): :recycle: use EffectTS to allow multiple transcription providers

--- libs/ai/src/media.ts
+++ libs/ai/src/media.ts
@@ -1,33 +1,97 @@
-'use server'
 import { createClient, srt, webvtt } from '@deepgram/sdk'
+import { Context, Data, Effect, Layer, ManagedRuntime } from 'effect'
 
-const deepgram = createClient(process.env['DEEPGRAM_API_KEY'] || '')
-// ! mess between File, Blob, and Buffer types
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function getTranscript(file: any, keywords?: string[], language?: string) {
-  const { result } = await deepgram.listen.prerecorded.transcribeFile(file, {
-    model: 'nova-2',
-    smart_format: true,
-    utterances: true,
-    numerals: true,
-    punctuate: true,
-    paragraphs: true,
-    ...(language ? { language } : { detect_language: true }),
-    keywords,
-  })
+interface TranscriptOutput {
+  srt: string
+  vtt: string
+  raw?: string
+  metadata: { duration: number }
+}
 
-  // TODO: Handle errors, possibly with EffectTS
-  if (!result) {
-    return { srt: '', vtt: '', raw: '' }
+export class Config extends Context.Tag('Config')<Config, { language: string; keywords: string[] }>() {
+  static readonly Test = {
+    language: '',
+    keywords: [],
   }
+}
 
-  // * Remove unnecessary metadata
-  const vtt = webvtt(result).replace(/WEBVTT\s*\n(?:[\s\S]*?)(?=^(?:\d{2}:)?\d{2}:\d{2})/m, 'WEBVTT\n\n')
+export class MediaFile extends Context.Tag('MediaFile')<MediaFile, Buffer>() {
+  static readonly Test = Buffer.from('')
+}
 
-  return {
-    srt: srt(result),
-    vtt,
-    raw: result?.results?.channels?.[0]?.alternatives?.[0]?.transcript,
-    metadata: result.metadata,
-  }
+const deepgramClient = createClient(process.env['DEEPGRAM_API_KEY'] || '')
+
+class FetchError extends Data.TaggedError('FetchError')<{}> {}
+class TranscribeError extends Data.TaggedError('TranscribeError')<{}> {}
+
+const deepgram = Effect.succeed({
+  getTranscript: Effect.gen(function* () {
+    const { language, keywords } = yield* Config
+    const file = yield* MediaFile
+    const { result } = yield* Effect.tryPromise({
+      try: () =>
+        deepgramClient.listen.prerecorded.transcribeFile(file, {
+          model: 'nova-2',
+          smart_format: true,
+          utterances: true,
+          numerals: true,
+          punctuate: true,
+          paragraphs: true,
+          ...(language ? { language } : { detect_language: true }),
+          keywords,
+        }),
+      catch: () => new FetchError(),
+    })
+
+    if (!result) {
+      return yield* new TranscribeError()
+    }
+
+    // * Remove unnecessary metadata
+    const vtt = webvtt(result).replace(/WEBVTT\s*\n(?:[\s\S]*?)(?=^(?:\d{2}:)?\d{2}:\d{2})/m, 'WEBVTT\n\n')
+
+    return {
+      srt: srt(result),
+      vtt,
+      raw: result?.results?.channels?.[0]?.alternatives?.[0]?.transcript,
+      metadata: result.metadata,
+    }
+  }),
+})
+
+export class TranscribeApi extends Context.Tag('TranscribeApi')<
+  TranscribeApi,
+  Effect.Effect.Success<typeof deepgram>
+>() {
+  static readonly Deepgram = Layer.effect(this, deepgram)
+}
+
+// TODO: Remove once we get rid of the old getTranscript function
+const program = Effect.gen(function* (_) {
+  const transcribeApi = yield* TranscribeApi
+  return yield* transcribeApi.getTranscript
+})
+
+const MainLayer = Layer.mergeAll(TranscribeApi.Deepgram)
+
+const TranscribeRuntime = ManagedRuntime.make(MainLayer)
+
+/**
+ * @deprecated Use TranscribeApi instead, this is just compability layer for now.
+ */
+export function getTranscript(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- deepgram sdk is not typed correctly
+  file: any,
+  keywords: string[] = [],
+  language: string = 'en',
+): Promise<TranscriptOutput> {
+  return TranscribeRuntime.runPromise(
+    program.pipe(
+      Effect.provideService(Config, {
+        language,
+        keywords,
+      }),
+      Effect.provideService(MediaFile, file),
+    ),
+  )
 }
