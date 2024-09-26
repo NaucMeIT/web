f772d36ce8f893cc3ad9dc6acd6bed164e9e4743
Petr Glaser <syreanis+1@gmail.com>
2024-09-26 14:54:44 +0200

    fix: export getTranscript as server action

--- libs/ai/src/index.ts
+++ libs/ai/src/index.ts
@@ -1,3 +1,5 @@
 export * from './generate'
-export * from './media'
 export * from './document'
+
+// ? In order to use functions in Next.js we need it in file with "use server" and we removed it from media.ts
+export * from './serverActions'
---
+++ libs/ai/src/serverActions.ts
@@ -0,0 +1,5 @@
+'use server'
+
+import { getTranscript } from './media'
+
+export { getTranscript }
