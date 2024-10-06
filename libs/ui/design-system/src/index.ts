ac911a1542b3910038437ae838336504274f7a03
Petr Glaser <syreanis+1@gmail.com>
2024-10-06 15:12:23 +0200

    feat(ui): add copy wrapper component

--- libs/ui/design-system/src/index.ts
+++ libs/ui/design-system/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/accordion'
 export * from './lib/button'
 export * from './lib/checkbox'
+export * from './lib/copy-wrapper'
 export * from './lib/input'
 export * from './lib/textarea'
