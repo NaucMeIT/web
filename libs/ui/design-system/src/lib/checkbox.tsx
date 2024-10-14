3a678ec3fe6fbeb07cfbf8eadca5898aa344b350
devodii <emmanuelodii80@gmail.com>
2024-10-14 03:44:40 +0100

    feat: introduce checkbox variants placeholders

--- libs/ui/design-system/src/lib/checkbox.tsx
+++ libs/ui/design-system/src/lib/checkbox.tsx
@@ -1,17 +1,32 @@
-import { Checkbox as CheckboxPrimitive } from '@nmit-coursition/ui/primitives/checkbox'
+import * as CheckboxPrimitive from '@nmit-coursition/ui/primitives/checkbox'
 import { Label } from '@nmit-coursition/ui/primitives/label'
+import { cn } from '@nmit-coursition/ui/utils'
+import { type VariantProps, cva } from 'class-variance-authority'
 
-interface CheckboxProps {
+const checkboxVariants = cva('', {
+  variants: {
+    variant: {
+      outlined: '',
+      critical: '', // applies a red border
+    },
+    size: {
+      sm: '', // 8px
+      lg: '', // 14px
+    },
+  },
+})
+
+interface CheckboxProps extends CheckboxPrimitive.RootProps, VariantProps<typeof checkboxVariants> {
   id: string
   label: string
   disabled?: boolean
   subtext?: string
 }
 
-export function Checkbox({ id, label, subtext, disabled }: CheckboxProps) {
+export const Checkbox = ({ id, label, subtext, disabled, className, variant, ...rest }: CheckboxProps) => {
   return (
     <div className='items-top flex space-x-2'>
-      <CheckboxPrimitive id={id} name={id} disabled={disabled} />
+      <CheckboxPrimitive.Main id={id} name={id} disabled={disabled} {...rest} className={cn({ variant, className })} />
       <div className='grid gap-1.5 leading-none'>
         <Label htmlFor={id}>{label}</Label>
         {subtext && <p className='text-sm text-muted-foreground'>{subtext}</p>}
