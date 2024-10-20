f75274400482538a0d409427dc4f00fe87ccae37
devodii <emmanuelodii80@gmail.com>
2024-10-20 20:57:01 +0100

    refactor(ui): improve textarea composability

--- libs/ui/design-system/src/lib/textarea.tsx
+++ libs/ui/design-system/src/lib/textarea.tsx
@@ -1,30 +1,28 @@
 import { Label } from '@nmit-coursition/ui/primitives/label'
 import { Textarea as TextareaPrimitive } from '@nmit-coursition/ui/primitives/textarea'
 import { cn } from '@nmit-coursition/ui/utils'
+import { splitProps, type MixinProps, type OverrideProps } from '@nmit-coursition/utils'
+import type { ComponentPropsWithRef } from 'react'
 
-interface TextareaWithTextProps extends React.ComponentProps<'textarea'> {
-  id: string
-  label: string
-  placeholder: string
+interface TextareaProps
+  extends OverrideProps<ComponentPropsWithRef<typeof TextareaPrimitive>, { id: string; placeholder: string }>,
+    MixinProps<'label', Omit<ComponentPropsWithRef<typeof Label>, 'htmlFor' | 'children'>>,
+    MixinProps<'subtext', ComponentPropsWithRef<'p'>>,
+    MixinProps<'container', ComponentPropsWithRef<'div'>> {
   subtext?: string
-  disabled?: boolean
-  containerClassName?: string
+  label: string
 }
 
-export function Textarea({
-  id,
-  label,
-  placeholder,
-  subtext,
-  disabled,
-  containerClassName,
-  ...rest
-}: TextareaWithTextProps) {
+export function Textarea({ id, label: labelText, placeholder, subtext: description, ...mixProps }: TextareaProps) {
+  const { container, label, subtext, rest } = splitProps(mixProps, 'label', 'subtext', 'container')
+
   return (
-    <div className={cn('grid w-full gap-1.5', containerClassName)}>
-      <Label htmlFor={id}>{label}</Label>
-      <TextareaPrimitive placeholder={placeholder} id={id} name={id} disabled={disabled} {...rest} />
-      {subtext && <p className='text-sm text-muted-foreground'>{subtext}</p>}
+    <div className={cn('grid w-full gap-1.5', container.className)} {...container}>
+      <Label htmlFor={id} {...label}>
+        {labelText}
+      </Label>
+      <TextareaPrimitive placeholder={placeholder} id={id} name={id} {...rest} />
+      {description && <p className={cn('text-sm text-muted-foreground', subtext.className)}>{description}</p>}
     </div>
   )
 }
