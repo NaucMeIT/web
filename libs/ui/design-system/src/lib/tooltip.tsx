a48f420fc487fa2e7b6838caf67c833b0dd7b0f1
devodii <emmanuelodii80@gmail.com>
2024-10-21 21:56:23 +0100

    feat(ui): add tooltip primitive and main abstraction

---
+++ libs/ui/design-system/src/lib/tooltip.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import * as TooltipPrimitive from '@nmit-coursition/ui/primitives/tooltip'
+import { splitProps, type MixinProps } from '@nmit-coursition/utils'
+import type { ComponentProps } from 'react'
+
+interface TriggerProps extends ComponentProps<typeof TooltipPrimitive.TooltipTrigger> {}
+
+interface ContentProps extends ComponentProps<typeof TooltipPrimitive.TooltipContent> {}
+
+interface Props
+  extends ComponentProps<typeof TooltipPrimitive.Tooltip>,
+    MixinProps<'trigger', Omit<TriggerProps, 'children'>>,
+    MixinProps<'content', Omit<ContentProps, 'children'>> {
+  trigger: TriggerProps['children']
+  content: ContentProps['children']
+}
+
+export const Tooltip = ({ trigger, content, ...mixProps }: Props) => {
+  const forwardedProps = splitProps(mixProps, 'trigger', 'content')
+
+  return (
+    <TooltipPrimitive.TooltipProvider>
+      <TooltipPrimitive.Tooltip {...forwardedProps.rest}>
+        <TooltipPrimitive.TooltipTrigger {...forwardedProps.trigger}>{trigger}</TooltipPrimitive.TooltipTrigger>
+        <TooltipPrimitive.TooltipContent {...forwardedProps.content}>{content}</TooltipPrimitive.TooltipContent>
+      </TooltipPrimitive.Tooltip>
+    </TooltipPrimitive.TooltipProvider>
+  )
+}
