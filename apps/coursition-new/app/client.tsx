c8e67076eef4df5112531a125afd39334b0ca1b5
Petr Glaser <syreanis+1@gmail.com>
2024-11-20 00:24:08 +0100

    fix(ui): delete vite based coursition-new

--- apps/coursition-new/app/client.tsx
+++
@@ -1,8 +0,0 @@
-import { StartClient } from '@tanstack/start'
-/// <reference types="vinxi/types/client" />
-import { hydrateRoot } from 'react-dom/client'
-import { createRouter } from './router'
-
-const router = createRouter()
-
-hydrateRoot(document, <StartClient router={router} />)
