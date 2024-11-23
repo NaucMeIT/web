5575a41622f5b8fecbf04beb82a3ed8e056a027c
Petr Glaser <syreanis+1@gmail.com>
2024-11-23 23:04:38 +0100

    feat(legacy): :chart_with_upwards_trend: add posthog analytics to know our users

--- apps/legacy_nmit/pages/_app.tsx
+++ apps/legacy_nmit/pages/_app.tsx
@@ -1,11 +1,43 @@
-import "../styles/global.css"
 import type { AppProps } from "next/app"
+import { Router } from 'next/router'
 import { SessionProvider } from "next-auth/react"
-import { Session } from "next-auth"
+import type { Session } from "next-auth"
 import splitbee from "@splitbee/web"
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
+import posthog from "posthog-js"
+import { PostHogProvider } from 'posthog-js/react'
+
+import "../styles/global.css"
 
 function MyApp({ Component, pageProps }: Readonly<AppProps<{ readonly session: Session }>>) {
+  const oldUrlRef = useRef('')
+
+  useEffect(() => {
+    posthog.init(process.env["NEXT_PUBLIC_POSTHOG_KEY"] || "", {
+      api_host: "/ingest",
+      ui_host: 'https://eu.posthog.com',
+      person_profiles: 'always',
+      // Enable debug mode in development
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug()
+      }
+    })
+
+    const handleRouteChange = () => posthog?.capture('$pageview')
+
+    const handleRouteChangeStart = () => posthog?.capture('$pageleave', {
+      $current_url: oldUrlRef.current
+    })
+
+    Router.events.on('routeChangeComplete', handleRouteChange);
+    Router.events.on('routeChangeStart', handleRouteChangeStart);
+
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+      Router.events.off('routeChangeStart', handleRouteChangeStart);
+    }
+  }, [])
+
     useEffect(() => {
         splitbee.init({
             scriptUrl: "/bee.js",
@@ -15,11 +47,14 @@ function MyApp({ Component, pageProps }: Readonly<AppProps<{ readonly session: S
     }, [])
 
     return (
+      <PostHogProvider client={posthog}>
         <SessionProvider session={pageProps.session}>
             <Component {...pageProps} />
-            <div id='calendly'></div>
+            <div id='calendly' />
         </SessionProvider>
+      </PostHogProvider>
     )
 }
+
 export default MyApp
 export { reportWebVitals } from "next-axiom"
