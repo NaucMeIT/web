93a0771d3703f9061c126c30d6cf6f40771f310d
Petr Glaser <syreanis+1@gmail.com>
2024-12-15 01:30:33 +0100

    refactor: :fire: remove unused coursition variants

--- apps/coursition/src/app/(auth)/error/page.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-
-export default function AuthErrorPage() {
-  return (
-    <div className='flex items-center justify-center h-screen w-screen'>
-      Sorry, an error occured during authentication
-    </div>
-  )
-}
--- apps/coursition/src/app/(auth)/sign-up/page.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-import { SignUp } from '../../../components/signUp'
-
-export default function SignUpPage() {
-  return <SignUp />
-}
