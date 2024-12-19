1302daf3e302669bd68c31790e5f5929bc881cc4
Petr Glaser <syreanis+1@gmail.com>
2024-12-19 21:08:45 +0100

    chore: update deps and rename files

--- apps/coursition/app/router.tsx
+++ apps/coursition/app/router.tsx
@@ -2,8 +2,8 @@ import { QueryClient } from '@tanstack/react-query'
 import { createRouter as createTanStackRouter } from '@tanstack/react-router'
 import { routerWithQueryClient } from '@tanstack/react-router-with-query'
 
-import { DefaultCatchBoundary } from './components/DefaultCatchBoundary'
-import { NotFound } from './components/NotFound'
+import { DefaultCatchBoundary } from './components/defaultCatchBoundary'
+import { NotFound } from './components/notFound'
 import { routeTree } from './routeTree.gen'
 
 export function createRouter() {
