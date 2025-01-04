4d4a1982ee9413ffc59c800582086098f8f11244
Petr Glaser <syreanis+1@gmail.com>
2025-01-04 18:56:42 +0100

    refactor(api): remove order legacy code

--- libs/api/dev/src/api-dev.ts
+++ libs/api/dev/src/api-dev.ts
@@ -14,32 +14,3 @@ export const apiDev = new Elysia({ prefix: '/dev', tags: ['dev'] })
   .get('/report-usage', async () => await computeUsage({ organisationId: 1 }), {
     afterResponse: ({ request }) => reportSpend({ request }),
   })
-  .get('/credit-order', async () => {
-    console.log('API KEY', process.env['BRJ_API_KEY'])
-    const response = await fetch(`https://brj.app/api/v1/shop/order/create?apiKey=${process.env['BRJ_API_KEY']}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        apiKey: process.env['BRJ_API_KEY'],
-        customer: { email: 'syreanis@gmail.com' },
-        locale: 'cs',
-        currency: 'CZK',
-        internalNotice: 'Nabití kreditů',
-        items: [
-          {
-            label: 'Nabití kreditů',
-            price: 300,
-            count: 1,
-            vat: 0,
-            creditAmount: 500,
-          },
-        ],
-        tags: { category: 'credit' },
-        returnUrl: 'http://localhost:3000',
-        formData: { code: 'objednavka-zaplacena', data: { field: 'true', 'field-2': 'option1' } },
-      }),
-    })
-    return { status: 'CREDITS', response: await response.json() }
-  })
--- libs/order/src/api-order.ts
+++ libs/order/src/api-order.ts
@@ -1,56 +1,32 @@
-import { apiCommonGuard, getLoggedUserOrThrow } from '@nmit-coursition/api/utils'
-import { prisma } from '@nmit-coursition/db'
-import { Elysia, t } from 'elysia'
-import { buyCredit, buySubscribe } from './buy'
-import { checkPayment } from './payment'
+import { apiCommonGuard } from '@nmit-coursition/api/utils'
+import { Elysia } from 'elysia'
 
-export const apiOrder = new Elysia({ prefix: '/order' })
-  .post(
-    '/webhook-check-payment-status',
-    async ({ body }) => {
-      const gateway_id = body.meta.custom_data.gateway_id
-      const payment = await prisma.shop__order_payment.findFirst({
-        select: { id: true },
-        where: {
-          gateway_id,
-          gateway_provider_id: 1,
+export const apiOrder = new Elysia({ prefix: '/order' }).use(apiCommonGuard).post('/credit-order', async () => {
+  const response = await fetch(`https://brj.app/api/v1/shop/order/create?apiKey=${process.env['BRJ_API_KEY']}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      apiKey: process.env['BRJ_API_KEY'],
+      customer: { email: 'syreanis@gmail.com' },
+      locale: 'cs',
+      currency: 'CZK',
+      publicNotice: 'Poznámka zákazníka',
+      internalNotice: 'Nabití kreditů',
+      items: [
+        {
+          label: 'Nabití kreditů',
+          price: 300,
+          count: 1,
+          vat: 0,
+          creditAmount: 500,
         },
-      })
-      if (!payment) {
-        throw new Error(`Payment not found: ${gateway_id}`)
-      }
-      await checkPayment(payment.id, body.data.attributes.status)
-
-      return { success: true }
-    },
-    {
-      body: t.Object({
-        meta: t.Object({
-          custom_data: t.Object({
-            gateway_id: t.String(),
-          }),
-        }),
-        data: t.Object({
-          attributes: t.Object({
-            status: t.String(),
-          }),
-        }),
-      }),
-    },
-  )
-  .use(apiCommonGuard)
-  .post('/buy-subscribe', async ({ request }) => {
-    const user = await getLoggedUserOrThrow(request)
-
-    return {
-      redirectUrl: await buySubscribe(user.id),
-    }
+      ],
+      // Tam budu přesměrován po úspěšném dokončení platby (děkovací stránky)
+      returnUrl: 'http://localhost:3000',
+    }),
   })
-  .post('/buy-credit', async ({ request }) => {
-    const user = await getLoggedUserOrThrow(request)
-    const credit = 500
-
-    return {
-      redirectUrl: await buyCredit(user.id, credit),
-    }
-  })
+  // Vždy přesměřovat na response.redirectUrl
+  return { status: 'CREDITS', response: await response.json() }
+})
