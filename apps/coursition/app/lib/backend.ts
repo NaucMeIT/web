eae4c23a90cfe7f680cab00706e6bc03bc22c2a7
Petr Glaser <syreanis+1@gmail.com>
2025-01-14 00:23:18 +0100

    refactor: delete vite-shit coursition

--- apps/coursition/app/lib/backend.ts
+++
@@ -1,8 +0,0 @@
-import { treaty } from '@elysiajs/eden'
-import { publicConfig } from '@nmit-coursition/env'
-import { Effect } from 'effect'
-import type { App } from '../../../backend/src/index'
-
-const typedPublic = Effect.runSync(publicConfig)
-const backendUrl = typedPublic.BACKEND_URL.href || `http://localhost:${typedPublic.BACKEND_PORT}`
-export const app = treaty<App>(backendUrl)
--- apps/coursition/app/liveblocks.config.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@liveblocks/client'
-import { createRoomContext } from '@liveblocks/react'
-
-const client = createClient({
-  publicApiKey: import.meta.env['VITE_LIVEBLOCKS_PUBLIC_API_KEY'],
-})
-
-export const { RoomProvider, useRoom } = createRoomContext(client)
