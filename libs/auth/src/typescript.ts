e841e1d7aa8bce60eafafc8ad6046f8a8fa281b9
Jan Barášek <janbarasek@gmail.com>
2025-01-25 12:22:24 +0100

    refactor: Remove legacy logic, use BRJ deps only.

--- libs/auth/src/typescript.ts
+++ libs/auth/src/typescript.ts
@@ -7,33 +7,3 @@ export interface User {
   createdAt: string
   updatedAt: string
 }
-
-export interface UserProfileRawRecord {
-  user_id: bigint
-  organisation_name: string
-  organisation_slug: string
-  email?: string
-  inserted_date: Date
-  updated_date: Date
-  synced_date: Date
-  full_name?: string
-  avatar_url?: string
-  credit: number
-  credit_alert_lower_then?: number
-  used_count_today: number
-}
-
-export interface UserProfileResponse {
-  internalId: number
-  organisationName: string
-  organisationSlug: string
-  email?: string
-  insertedDate: Date
-  updatedDate: Date
-  syncedDate: Date
-  fullName?: string
-  avatarUrl?: string
-  credit: number
-  creditAlertLowerThen?: number
-  usedCountToday: number
-}
