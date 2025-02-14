d14892fafd95afa84066cf0c2bff3e76abc31396
Petr Glaser <syreanis+1@gmail.com>
2025-02-14 15:15:28 +0100

    refactor: start migration to modernjs with TW4

--- apps/coursition-modern/modern.config.ts
+++ apps/coursition-modern/modern.config.ts
@@ -1,23 +1,43 @@
+import { SemiRspackPlugin } from '@douyinfe/semi-rspack-plugin'
 import { appTools, defineConfig } from '@modern-js/app-tools'
 import { tailwindcssPlugin } from '@modern-js/plugin-tailwindcss'
 import { loadEnv } from '@rsbuild/core'
+import { pluginImageCompress } from '@rsbuild/plugin-image-compress'
 import { pluginNodePolyfill } from '@rsbuild/plugin-node-polyfill'
 
 const { publicVars } = loadEnv({ cwd: '../..' })
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
+  output: {
+    polyfill: 'usage',
+  },
   source: {
     define: publicVars,
   },
   runtime: {
     router: true,
   },
-  builderPlugins: [pluginNodePolyfill()],
+  builderPlugins: [pluginNodePolyfill(), pluginImageCompress(['jpeg', 'png', 'ico', 'avif', 'svg'])],
   plugins: [
     appTools({
-      bundler: 'rspack', // Set to 'webpack' to enable webpack
+      bundler: 'rspack',
     }),
     tailwindcssPlugin(),
   ],
+  tools: {
+    rspack: {
+      cache: true,
+      experiments: {
+        topLevelAwait: true,
+        incremental: true,
+        futureDefaults: true,
+      },
+      plugins: [
+        new SemiRspackPlugin({
+          cssLayer: true,
+        }),
+      ],
+    },
+  },
 })
