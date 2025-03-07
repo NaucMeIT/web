1875ece9faab4e3355d17b1271e8255155777966
Petr Glaser <syreanis+1@gmail.com>
2025-03-08 00:29:11 +0100

    refactor: delete tanstack-based coursition due to bundle size issues

--- apps/coursition/src/lib/backend.ts
+++
@@ -1,8 +0,0 @@
-import { treaty } from '@elysiajs/eden'
-import type { TypedApp } from '@nmit-coursition/api/full'
-import { publicConfig } from '@nmit-coursition/env'
-import { Effect } from 'effect'
-
-const typedPublic = Effect.runSync(publicConfig)
-const backendUrl = typedPublic.BACKEND_URL.href || `http://localhost:${typedPublic.BACKEND_PORT}`
-export const app = treaty<TypedApp>(backendUrl)
